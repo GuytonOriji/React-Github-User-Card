434e2a382fd07d165e97a9af52e5e0188f17233f
thisguycodez <guyton578@gmail.com>
2020-02-11 18:46:48 -0500

    Updates

---
+++ precache-manifest.df09fd3189e1298a4b092be82c7a6345.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3f7003f28cdb7dc8c2bc117f6d98aa",
+    "url": "/React-Github-User-Card/index.html"
+  },
+  {
+    "revision": "e51bb71136a8aec5ffd4",
+    "url": "/React-Github-User-Card/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dd1dc2c1c76da8fbb2b0",
+    "url": "/React-Github-User-Card/static/css/main.1d592efb.chunk.css"
+  },
+  {
+    "revision": "e51bb71136a8aec5ffd4",
+    "url": "/React-Github-User-Card/static/js/2.06192576.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/React-Github-User-Card/static/js/2.06192576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1dc2c1c76da8fbb2b0",
+    "url": "/React-Github-User-Card/static/js/main.b8c3047d.chunk.js"
+  },
+  {
+    "revision": "37dd0f7f6eb98d741f6c",
+    "url": "/React-Github-User-Card/static/js/runtime-main.7731c531.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Github-User-Card/static/media/logo.5d5d9eef.svg"
+  }
+]);
